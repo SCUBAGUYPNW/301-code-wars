107836d13a23cae6d5ae37d4251a84f24824fb47
Mitchel Hall <scubaphoto@gmail.com>
2017-12-04 18:27:41 -0800

    commiting on correct branch

--- abbreviate-a-two-word-name/solution.js
+++ abbreviate-a-two-word-name/solution.js
@@ -1,5 +1,5 @@
 function abbrevName(name){
-    let firstLetters = name.split(' ').map(function (s) { return s.charAt(0); }).join('.');
-    let capLetters = firstLetters.toUpperCase();
+    let isolateFirstLetters = name.split(' ').map(function (s) { return s.charAt(0); }).join('.');
+    let capLetters = isolateFirstLetters.toUpperCase();
     return capLetters;
     }
